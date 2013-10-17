4ad0b8fcc11706394c1c3bc6d5eff871002120a6
benkrikler <mr.krikler@googlemail.com>
2013-10-17 14:47:39 +0100

    Added the functionality to the Plotify.js script to handle arguments from the URL. To sumbit arguments, place a '?' (question mark) after the URL, followed by a list of key=value pairs separated with a '&' (ampersand). Keys should be the name of a parameter and values should be the value to set it to

--- Plotify.js
+++ Plotify.js
@@ -15,7 +15,6 @@ function Image(name,alt,parameter_dependency,display_extension,download_extensio
 // display_extension (string): File extension of the images on the webpage
 // download_extension [optional] (string): File extension of the images that download when clicked. 
 //          If ommitted, the value of 'display_extension' is used.
-// name: 
     //----------------- Data members -----------------//
     this.Name=name;
     this.Title=alt;
@@ -29,43 +28,43 @@ function Image(name,alt,parameter_dependency,display_extension,download_extensio
     //----------------- writeHtml method -----------------//
     // Function to write the Html code for the image
     this.writeHtml=function(cssClass,DivID){
-	var html="<a id="+this.DownloadID+" >\n";
-	if(cssClass) html+=" <img class='"+cssClass+"'";
-	else html+=" <img class='plot'";
-	html+=" id='"+this.DisplayID+"'";
-	html+=" alt='"+this.Title+"'";
-	html+=" title='"+this.Title+"'";
-	html+="/></a>";
+        var html="<a id="+this.DownloadID+" >\n";
+        if(cssClass) html+=" <img class='"+cssClass+"'";
+        else html+=" <img class='plot'";
+        html+=" id='"+this.DisplayID+"'";
+        html+=" alt='"+this.Title+"'";
+        html+=" title='"+this.Title+"'";
+        html+="/></a>";
 
-	// Define the target for the parameter table.
-	// If DivID is supplied, use that, else use the parameter's name as the ID of the block.
-	var target_div=typeof DivID !=='undefined' ? DivID: this.Name;
+        // Define the target for the parameter table.
+        // If DivID is supplied, use that, else use the parameter's name as the ID of the block.
+        var target_div=typeof DivID !=='undefined' ? DivID: this.Name;
 
-	// Place html for the parameter table into the parameter's div
-	var section=document.getElementById(target_div);
-	if(!section) {
-	    alert("Unable to find div with ID:"+ target_div+ " in Image.writeHtml")
-		return;
-	}
-	section.innerHTML+=html;
+        // Place html for the parameter table into the parameter's div
+        var section=document.getElementById(target_div);
+        if(!section) {
+            alert("Unable to find div with ID:"+ target_div+ " in Image.writeHtml")
+                return;
+        }
+        section.innerHTML+=html;
     }
 
     //----------------- update method -----------------//
     // Function to update the image
     this.update=function(ChangedParam){
-	// Check image depends on the parameter (to avoid downloading a new copy of the same image)
-	if(ChangedParam && this.Dependencies.indexOf(ChangedParam)==-1) return;
+        // Check image depends on the parameter (to avoid downloading a new copy of the same image)
+        if(ChangedParam && this.Dependencies.indexOf(ChangedParam)==-1) return;
 
-	// Convert the dependency array to a string (uses the overriden toString method in the Parameter class)
-	var filename=this.Dependencies.join("");
+        // Convert the dependency array to a string (uses the overriden toString method in the Parameter class)
+        var filename=this.Dependencies.join("");
 
-	var section=document.getElementById(this.DisplayID)
-	if(!section) {
-	    alert("Unable to find image html for image: "+ this.Name+ "\rPerhaps you haven't called writeHtml on this image");
-	    return;
-	}
-	section.setAttribute('src',filename+"."+this.DisplayExt);
-	document.getElementById(this.DownloadID).setAttribute('href',filename+"."+this.DownloadExt);
+        var section=document.getElementById(this.DisplayID)
+            if(!section) {
+                alert("Unable to find image html for image: "+ this.Name+ "\rPerhaps you haven't called writeHtml on this image");
+                return;
+            }
+        section.setAttribute('src',filename+"."+this.DisplayExt);
+        document.getElementById(this.DownloadID).setAttribute('href',filename+"."+this.DownloadExt);
     }
 
     //----------------- Register this image -----------------//
@@ -93,70 +92,80 @@ function Parameter(name,values,filenames){
     //----------------- writeHtml -----------------//
     // Create the html code for the Parameter
     this.writeHtml=function(startup,columns,divID){
-    // startup (integer): An integer giving the index of the startup value for the parameter.
-    //                    Must be in the range [0,values.length[
-    // columns (integer): An integer specifying the number of columns to use in the parameter value table.
-    // divID [optional]: The ID of the div block that should contain the parameter.  
-    //                    If not supplied, the parameter's name (this.Name) is used instead.
-	this.CurrentVal=startup;
+        // startup (integer): An integer giving the index of the startup value for the parameter.
+        //                    Must be in the range [0,values.length[
+        // columns (integer): An integer specifying the number of columns to use in the parameter value table.
+        // divID [optional]: The ID of the div block that should contain the parameter.  
+        //                    If not supplied, the parameter's name (this.Name) is used instead.
+        this.CurrentVal=startup;
 
-	// Create html for the table
-	var html="<table class='value_table' id='tab_"+this.Name+"'>";
-	for(i=0;i<this.Values.length;){
-	    html=html.concat("<tr>");
-	    for(var j=0;j<columns;j++){
-		html=html.concat("<td>");
-		html=html.concat("<a id="+this.getValueID(i)+" href=javascript:"+this.Name+".changeValue("+i+")> "+this.Values[i]+" </a>");
-		html=html.concat("</td>");
-		i++; //Increment parameter counter as we've added another value
-		if(i>this.Values.length) break; // Not all rows may be complete
-	    }
-	    html=html.concat("</tr>");
-	}
-	html=html.concat("</table>");
+        // Create html for the table
+        var html="<table class='value_table' id='tab_"+this.Name+"'>";
+        for(i=0;i<this.Values.length;){
+            html=html.concat("<tr>");
+            for(var j=0;j<columns;j++){
+                html=html.concat("<td>");
+                html=html.concat("<a id="+this.getValueID(i)+" href=javascript:"+this.Name+".changeValue("+i+")> "+this.Values[i]+" </a>");
+                html=html.concat("</td>");
+                i++; //Increment parameter counter as we've added another value
+                if(i>this.Values.length) break; // Not all rows may be complete
+            }
+            html=html.concat("</tr>");
+        }
+        html=html.concat("</table>");
 
-	// Define the target for the parameter table.
-	// If divID is supplied, use that, else use the parameter's name as the ID of the block.
-	var target_div=typeof divID !=='undefined' ? divID: this.Name;
+        // Define the target for the parameter table.
+        // If divID is supplied, use that, else use the parameter's name as the ID of the block.
+        var target_div=typeof divID !=='undefined' ? divID: this.Name;
 
-	// Place html for the parameter table into the parameter's div
-	var section=document.getElementById(target_div);
-	if(!section) {
-	    alert("Unable to find div with ID:"+ target_div+ " in Image.writeHtml")
-		return;
-	}
-	section.innerHTML+=html;
+        // Place html for the parameter table into the parameter's div
+        var section=document.getElementById(target_div);
+        if(!section) {
+            alert("Unable to find div with ID:"+ target_div+ " in Image.writeHtml")
+                return;
+        }
+        section.innerHTML+=html;
 
-	//Set the link styles
-	this.setLinks()
+        //Set the link styles
+        this.setLinks()
     }
 
     //----------------- changeValue -----------------//
     // This function is used as the href target for the parameter value links
     this.changeValue=function(newVal){
-	this.CurrentVal=newVal;
-	this.setLinks();
-	ControllableElements.updateControllables(this);
+        this.CurrentVal=newVal;
+        this.setLinks();
+        ControllableElements.updateControllables(this);
     }
 
     //----------------- setLinks -----------------//
     // Function to set the style of the parameter values 
     // Called when the link is clicked
     this.setLinks=function(){
-	for(var i=0;i<this.Values.length;i++){
-	    if(i==this.CurrentVal){
-		document.getElementById(this.getValueID(i)).setAttribute('class','current_value');
-	    }else{
-		document.getElementById(this.getValueID(i)).setAttribute('class','value');
-	    }
-	}
+        for(var i=0;i<this.Values.length;i++){
+            if(i==this.CurrentVal){
+                document.getElementById(this.getValueID(i)).setAttribute('class','current_value');
+            }else{
+                document.getElementById(this.getValueID(i)).setAttribute('class','value');
+            }
+        }
     }
 
     //----------------- getValueID -----------------//
     //Helper function to produce the id for the requested cell in the parameter table
     this.getValueID=function(index){
-	return this.Name+"_"+index;
+        return this.Name+"_"+index;
     }
+
+    //----------------- getIndexOfValue -----------------//
+    //Helper function to produce the id for the requested cell in the parameter table
+    this.getIndexOfValue=function(value){
+        return this.Filenames.indexOf(value);
+    }
+
+    //----------------- Register this parameter -----------------//
+    // Register parameter on the ParameterList object
+    ParameterList[this.Name]=this;
 }
 
 //----------------- toString -----------------//
@@ -166,6 +175,11 @@ Parameter.prototype.toString=function(){
     return (this.Filenames[this.CurrentVal]);
 }
 
+//----------------- Parameter List -----------------//
+// An object to store all created parameters on (for ease of looping over all
+// parameters and setting values from the URL
+ParameterList=new Object();
+
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // 
 // Controllable Element List
@@ -173,25 +187,52 @@ Parameter.prototype.toString=function(){
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // Object to control a register of the controllable elements (images, tables, etc etc), that depend on the parameters.
 // Any time a parameter has its value changed, the parameter tells this object to update all images with the new value.
-ControllableElements = new function(){
-    // List of all images to loop over when a parameter is changed
-    this.ElementList=new Array();
+    ControllableElements = new function(){
+        // List of all images to loop over when a parameter is changed
+        this.ElementList=new Array();
 
-    // extend image list
-    this.addElement=function(element){
-	this.ElementList.push(element);
+        // extend image list
+        this.addElement=function(element){
+            this.ElementList.push(element);
+        }
+
+        // Function to alert all elements that a parameter has changed
+        this.updateControllables=function(ChangedParam){
+            // Loop over all images and tell them to update
+            for(i=0;i<this.ElementList.length;i++){
+                this.ElementList[i].update(ChangedParam);
+            }
+        }
     }
 
-    // Function to alert all elements that a parameter has changed
-    this.updateControllables=function(ChangedParam){
-	// Loop over all images and tell them to update
-	for(i=0;i<this.ElementList.length;i++){
-	    this.ElementList[i].update(ChangedParam);
-	}
+////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+//  Functions for loading the page
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////
+function ProcessUserOptions(){
+    // Get all the user supplied parameters
+    var parameters = location.search.substring(1);
+    if(parameters=="") {return;}
+    parameters = parameters.split("&");
+
+    // Iterate over each parameter and if such a parameter exists set it's
+    // current value to be the requested one
+    for (var i in parameters){
+        var input =parameters[i].split("=");
+        if(ParameterList.hasOwnProperty(input[0])){
+            var param=ParameterList[input[0]];
+            var index =param.getIndexOfValue(input[1]);
+            if(index>=0)param.changeValue(index);
+            else alert("Parameter '"+input[0]+"' has no value, '"+input[1]+"'")
+        } else { 
+            alert("Parameter '"+input[0]+"' not found.");
+        }
     }
 }
 
 window.onload=function(){
+    ProcessUserOptions();
     ControllableElements.updateControllables();
 }
 // -->
